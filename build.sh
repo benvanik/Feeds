cca5b9f18f5398378e05cf01cc4eea41797c14b2
Ben Vanik <ben.vanik@gmail.com>
2008-10-13 10:08:15 +0000

    Fixed library name. Better build script.

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+PROJECTNAME="Feeds"
+
 # Release
-xcodebuild -project Feeds.xcodeproj/ -target Feeds-iPhone -configuration Release -sdk iphonesimulator2.1
-xcodebuild -project Feeds.xcodeproj/ -target Feeds-iPhone -configuration Release -sdk iphoneos2.1
-xcodebuild -project Feeds.xcodeproj/ -target Feeds -configuration Release -sdk macosx10.5
+xcodebuild -project $PROJECTNAME.xcodeproj/ -target $PROJECTNAME-iPhone -configuration Release -sdk iphonesimulator2.1
+xcodebuild -project $PROJECTNAME.xcodeproj/ -target $PROJECTNAME-iPhone -configuration Release -sdk iphoneos2.1
+xcodebuild -project $PROJECTNAME.xcodeproj/ -target $PROJECTNAME -configuration Release -sdk macosx10.5
 
 # Documentation
-xcodebuild -project Feeds.xcodeproj/ -target Documentation -configuration Release -sdk macosx10.5
+xcodebuild -project $PROJECTNAME.xcodeproj/ -target Documentation -configuration Release -sdk macosx10.5
 
 # Consolidate into a single folder
 rm -rf Redist
@@ -14,17 +16,18 @@ mkdir Redist
 mkdir Redist/Documentation
 mkdir Redist/iPhone
 mkdir Redist/iPhone/include
-mkdir Redist/iPhone/include/Feeds
+mkdir Redist/iPhone/include/$PROJECTNAME
 mkdir Redist/iPhone/lib
 mkdir Redist/MacOS
 
 cp LICENSE Redist/
 
-cp build/Release-iphoneos/Feeds/* Redist/iPhone/include/Feeds
-cp build/Release-iphoneos/libFeeds-iPhone-iphoneos.a Redist/iPhone/lib/
-cp build/Release-iphonesimulator/libFeeds-iPhone-iphonesimulator.a Redist/iPhone/lib/
-cp -R build/Release/Feeds.framework Redist/MacOS/
+cp build/Release-iphoneos/$PROJECTNAME/* Redist/iPhone/include/$PROJECTNAME
+cp build/Release-iphoneos/lib$PROJECTNAME-iphoneos.a Redist/iPhone/lib/
+cp build/Release-iphonesimulator/lib$PROJECTNAME-iphonesimulator.a Redist/iPhone/lib/
+cp -R build/Release/$PROJECTNAME.framework Redist/MacOS/
+rm -Rf Redist/MacOS/$PROJECTNAME.framework/Versions/A/PrivateHeaders/*
 
-cp -R Documentation/DoxygenDocs.docset/html/org.noxa.Feeds.docset Redist/
+cp -R Documentation/DoxygenDocs.docset/html/org.noxa.$PROJECTNAME.docset Redist/
 cp -R Documentation/html/* Redist/Documentation/
-rm -Rf Redist/Documentation/org.noxa.Feeds.docset
+rm -Rf Redist/Documentation/org.noxa.$PROJECTNAME.docset
